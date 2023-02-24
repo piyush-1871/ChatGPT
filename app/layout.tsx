64999cbfe45a72f72e132dc0268098fc4a2eb915
piyush-1871 <piyush1871b@gmail.com>
2023-02-24 22:53:23 +0530

    Building the New Chat Component and SideBar(2/5)

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({
       <body>
         <div className="flex">
           {/* Sidebar */}
-          <Sidebar/> 
+          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+            <Sidebar />
+          </div>
 
           {/* ClientProvider- Notification */}
 
--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,8 +1,12 @@
-
+import { PlusIcon } from '@heroicons/react/24/solid'
 
 function NewChat() {
   return (
-    <div>NewChat</div>
+    <div className='border-gray-700 border chatRow'>
+        {/* PlusIcon */}
+        <PlusIcon className='h-4 w-4' />
+        <p>New Chat</p>
+    </div>
   )
 }
 
