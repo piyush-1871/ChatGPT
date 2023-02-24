f1a5bd62561ba599a3671730ad70dd8e3d4060d0
piyush-1871 <piyush1871b@gmail.com>
2023-02-24 22:36:49 +0530

    Building the HomePage(1/2)

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="flex">
+          {/* Sidebar */}
+
+          {/* ClientProvider- Notification */}
+
+          <div className="bg-[#343541] flex-1">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
