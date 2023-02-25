d72efcca24055bf3da26435c14948d377ad7f69c
piyush-1871 <piyush1871b@gmail.com>
2023-02-25 14:46:48 +0530

    Connecting to Cloud Firestore Database

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,10 +2,20 @@
 
 import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { collection } from "firebase/firestore";
+import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 
 
 function Sidebar() {
   const { data: session } = useSession();
+  const [chats, loading, error] = useCollection(
+    session && collection(db, "users", session.user?.email!, "chats")
+  );
+
+  console.log(chats);
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -16,7 +26,10 @@ function Sidebar() {
           <div>{/* Model Selection */}</div>
 
           {/* Map through the chatrows */}
-          
+          {chats?.docs.map(chat => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
+
         </div>
       </div>
       {session && (
