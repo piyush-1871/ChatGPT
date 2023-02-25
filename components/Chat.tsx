a7285a0aef1bf60668a18038248b771ead46be53
piyush-1871 <piyush1871b@gmail.com>
2023-02-25 17:05:41 +0530

    Building the Chat Input Component(1/2)

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 function Chat({chatId}: Props) {
   return (
-    <div className="">
+    <div className="flex-1">
 
     </div>
   )
--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,12 +1,37 @@
+"use client";
+
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
 type Props = {
-    chatId : string
-}
+  chatId: string;
+};
 
-
-function ChatInput({chatId}: Props) {
+function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
   return (
-    <div>ChatInput</div>
-  )
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
+      <form action="" className="p-5 space-x-5 flex">
+        <input
+          className="focus:outline-none bg-transparent flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Type your message here..."
+        />
+        <button disabled={!prompt || !session}
+        className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-600 disabled:cursor-not-allowed"
+        type="submit">
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
+
+      <div>{/* Model Selection */}</div>
+    </div>
+  );
 }
 
-export default ChatInput
+export default ChatInput;
