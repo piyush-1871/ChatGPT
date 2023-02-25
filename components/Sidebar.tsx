803d6c2b0f14986b01b0581053cabe4d37b5db2e
piyush-1871 <piyush1871b@gmail.com>
2023-02-25 13:57:32 +0530

    Building the SideBar Component(3/5)

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,26 +1,34 @@
-import NewChat from "./NewChat"
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import NewChat from "./NewChat";
 
 
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="p-2 flex flex-col h-screen">
-        <div className="flex-1">
-            <div className="">
+      <div className="flex-1">
+        <div className="">
+          {/* NewChat */}
+          <NewChat />
 
+          <div>{/* Model Selection */}</div>
 
-                {/* NewChat */}
-                <NewChat />
-
-                <div>
-                    {/* Model Selection */}
-                </div>
-
-                {/* Map through the chatrows */}
-
-            </div>
+          {/* Map through the chatrows */}
+          
         </div>
+      </div>
+      {session && (
+        <img
+          src={session.user?.image!}
+          alt="profile-pic"
+          onClick={() => signOut()}
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
