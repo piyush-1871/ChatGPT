b00465760480e8c4129951a4f5be37b75c60a461
piyush-1871 <piyush1871b@gmail.com>
2023-02-25 17:55:54 +0530

    Implementing Messaging Functionality(2/2)

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
-import { serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { FormEvent, useState } from "react";
+import { db } from "../firebase";
 
 type Props = {
   chatId: string;
@@ -13,23 +14,41 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
 
-  const sendMessage = async(e: FormEvent<HTMLFormElement>)=>{
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(!prompt) return;
+    if (!prompt) return;
 
     const input = prompt.trim();
     setPrompt("");
 
     const message: Message = {
-        text : input,
-        createdAt: serverTimestamp(),
-        user: {
-            _id: session?.user?.email!,
-            name: session?.user?.name!,
-            avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`,
-        }
-    }
-  }
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+
+    // Toast Notification
+
+    await fetch('/api/askQuestion')
+  };
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
       <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
@@ -41,9 +60,11 @@ function ChatInput({ chatId }: Props) {
           type="text"
           placeholder="Type your message here..."
         />
-        <button disabled={!prompt || !session}
-        className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-600 disabled:cursor-not-allowed"
-        type="submit">
+        <button
+          disabled={!prompt || !session}
+          className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-600 disabled:cursor-not-allowed"
+          type="submit"
+        >
           <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
         </button>
       </form>
