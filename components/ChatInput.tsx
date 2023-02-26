19bc189edabbd39fc39520ceb373006f7cef1e5e
piyush-1871 <piyush1871b@gmail.com>
2023-02-26 12:35:20 +0530

    Implementing React Hot Toast Library for Notifications

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -5,6 +5,7 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { FormEvent, useState } from "react";
 import { db } from "../firebase";
+import {toast} from 'react-hot-toast'
 
 type Props = {
   chatId: string;
@@ -13,6 +14,9 @@ type Props = {
 function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
+
+  // TODO:  useSWR to get model
+  const model = "text-davinci-003";
 
   const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -46,8 +50,25 @@ function ChatInput({ chatId }: Props) {
     );
 
     // Toast Notification
+    const notification = toast.loading('ChatGPT is thinking!');
 
-    await fetch('/api/askQuestion')
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(()=>{
+      // Toast Notification for successfull
+      toast.success('ChatGPT has responded!',{
+        id: notification,
+      })
+    });
   };
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
