848807cc18589940de1be43de7cbba2af6a7872d
piyush-1871 <piyush1871b@gmail.com>
2023-02-26 13:21:44 +0530

    Setting up OpenAI API Key(2/3)

---
+++ pages/api/askQuestion.ts
@@ -0,0 +1,31 @@
+import query from '../../lib/queryApi';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  answer: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const {prompt, chatId, model, session} = req.body;
+
+  if(!prompt){
+    res.status(400).json({ answer: 'Please provide a valid a prompt' });
+    return;
+  }
+  if(!chatId){
+    res.status(400).json({ answer: "Please provide a valid chat ID!" });
+    return;
+  }
+
+  // chatGPT Query
+  const response = await query(prompt, chatId, model);
+
+  const message: Message = {
+    text: response || "ChatGPT cannot fid the answer!"
+  }
+  res.status(200).json({ name: 'John Doe' })
+}
